bf61a910cc7a151c11a0c9170442acf80138ad18
Sebastian Pardo <sapardosuazo@gmail.com>
2020-04-10 17:49:13 -0400

    Fix error

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-l", lineas,"-fn", dmenufont, "-nb", "#2e2e2e", "-nf", "#FFFFFF", "-sb", "#49FF49", "-sf", "#1e1e1e", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-l", "15","-fn", dmenufont, "-nb", "#2e2e2e", "-nf", "#FFFFFF", "-sb", "#49FF49", "-sf", "#1e1e1e", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 #include <X11/XF86keysym.h>
